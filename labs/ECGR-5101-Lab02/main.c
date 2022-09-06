2d22ef2ac028e515e609ce4383b0ff3418a053d6
Grayson Bass <graysonbass@Graysons-MacBook-Pro.local>
2022-09-06 17:42:37 -0400

    Added finished lab2.

--- labs/ECGR-5101-Lab02/main.c
+++ labs/ECGR-5101-Lab02/main.c
@@ -18,12 +18,10 @@ int count=0,state; //Declare required variables
 int main(void) {
     WDTCTL = WDTPW | WDTHOLD;   // Stop watchdog timer
 
-    P1DIR&=~BIT3; //Set P1.3 as input
-    P1REN|=BIT3; //Enable pullup/pulldown resistors for P1.3
-    P1OUT|=BIT3; //Set P1.3 to have pull up resistors
+    P1DIR&=~BIT4; //Set P1.4 as input
 
-    P1IE|=BIT3; //Enable interrupt on P1.3
-    P1IES|=BIT3; //Set interrupt flag on the falling edge of logic level on P1.3
+    P1IE|=BIT4; //Enable interrupt on P1.4
+    P1IES|=BIT4; //Set interrupt flag on the falling edge of logic level on P1.4
 
     TACCR0=1000; //Make the timer count from 0 to 10000, which will take  ~0.001 seconds
     __enable_interrupt(); //Enable maskable interrupts
@@ -48,9 +46,9 @@ __interrupt void P1_Function()
 {
     count=0; //Reset count
     TACTL|=TASSEL_2+MC_1+TAIE; //Start Timer0 with SMCLK clock source, UP mode and enable overflow interrupt
-    state=(P1IN&BIT3)>>3; //Save the state of the switch
-    P1IE&=~BIT3; //Disable interrupt on P1.3, now the Timer will take care of Debouncing
-    P1IFG&=~BIT3; // Reset Port1 interrupt flag
+    state=(P1IN&BIT4)>>3; //Save the state of the switch
+    P1IE&=~BIT4; //Disable interrupt on P1.4, now the Timer will take care of Debouncing
+    P1IFG&=~BIT4; // Reset Port1 interrupt flag
 }
 
 #pragma vector=TIMER0_A1_VECTOR
@@ -60,35 +58,24 @@ __interrupt void TMR0()
                             //This would be required in projects where multiple interrupts have
                             //the same interrupt vector. Here it is only optional.
         {
-            if(state==((P1IN&BIT3)>>3)) //If the state of the LED is the same
+            if(state==((P1IN&BIT4)>>3)) //If the state of the LED is the same
                 count++; //Increment the counter variable
             else
             {
                 count=0; //If not same, reset the counter variable
-                state=((P1IN&BIT3)>>3); //And save the present state of the switch
+                state=((P1IN&BIT4)>>3); //And save the present state of the switch
             }
         if(count==10) //If the state has been consistently the same
             {
                 while(state==0)
                 {
-//                    if(state==0)
-//                    { //If the switch was pressed
                         P2OUT&= ~BIT0; //turn led off
-                        P1IE|=BIT3; //We have handled the debouncing, now we again enable interrupt on P1.3, for it to again detect switch bounce
+                        P1IE|=BIT4; //We have handled the debouncing, now we again enable interrupt on P1.4, for it to again detect switch bounce
                         TACTL=0; //Stop the Timer
                         TACTL|=TACLR; //Clear the Timer counter
-                        state = ((P1IN&BIT3)>>3);
-//                    }
-//                    else
-//                    {
-//                        break;
-//                    }
-
+                        state = ((P1IN&BIT4)>>3);
                 }
                 TACTL&=~(TAIFG); //Reset the interrupt flag
             }
         }
 }
-
-
-
